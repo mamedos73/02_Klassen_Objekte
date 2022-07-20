aea0b989e63117bddc2298ad12d6da31aece2c65
Peter Schmidt <schmidt.multimedia@gmail.com>
2022-07-20 08:39:13 +0200

    Update App.java

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -2,14 +2,7 @@ package com.cc.java;
 
 public class App {
     
-    public static void main(String[] args) {
-     
-    }
-
-    public static void output(String outputStr) {
-        System.out.println(outputStr);
-    }
-
+   
 
 
 }
